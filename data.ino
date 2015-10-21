cb53c3f2ae3f17f6dd6e1f658d80ce20148a4d5f
Kevin Purdy <kevin@ridewithgps.com>
2015-10-20 21:29:16 -0700

    Fixed for spec.

--- data.ino
+++ data.ino
@@ -9,65 +9,68 @@
  */
 
 typedef struct {
-  uint16_t count;
-  uint16_t noteOffset;
+    uint16_t count;
+    uint16_t noteOffset;
 } BarS;
 
 Data *load_data() {
-  Data *data = (Data*) malloc(sizeof(Data));
-  int noteLen = sizeof(NOTES) / 4;
+    Data *data = (Data *) malloc(sizeof(Data));
+    int noteLen = sizeof(NOTES) / 4;
 
-  Note *notes = (Note*) &NOTES;
-  BarS *raw_bars = (BarS*) &BARS;
+    int i, j = 0;
 
-  Bar *bars = (Bar*)malloc(sizeof(Bar) * BAR_COUNT);
+    Note *notes = (Note *) &NOTES;
+    BarS *raw_bars = (BarS *) &BARS;
 
-  Channel *channels = (Channel*)malloc(sizeof(Channel) * CHANNEL_COUNT);
-  Composition *programs = (Composition*)malloc(sizeof(Composition) * PROGRAM_COUNT);
+    Bar *bars = (Bar *) malloc(sizeof(Bar) * BAR_COUNT);
 
-  for (int i = 0; i < BAR_COUNT; i++) {
-    bars[i].count = raw_bars[i].count;
-    bars[i].notes = &notes[raw_bars[i].noteOffset];
-    bars[i].current = 0;
-  }
+    Channel *channels = (Channel *) malloc(sizeof(Channel) * CHANNEL_COUNT);
+    Composition *programs = (Composition *) malloc(sizeof(Composition) * PROGRAM_COUNT);
 
-  int counter = 0;
-  for (int i = 0; i < CHANNEL_COUNT; i++) {
-      char cnt = CHANNELS[counter++];
-      //counter += sizeof(uint16_t);
-      channels[i].count = cnt;
-      channels[i].current = 0;
-      channels[i].bars = (Bar**)malloc(sizeof(Bar*) * cnt);
-      for (int j = 0; j < cnt; j++) {
-        channels[i].bars[j] = &bars[CHANNELS[counter++]];
+    for (i = 0; i < BAR_COUNT; i++) {
+        bars[i].count = raw_bars[i].count;
+        bars[i].notes = &notes[raw_bars[i].noteOffset];
+        bars[i].current = 0;
+    }
+
+    int counter = 0;
+    for (i = 0; i < CHANNEL_COUNT; i++) {
+        char cnt = CHANNELS[counter++];
         //counter += sizeof(uint16_t);
-      }
-  }
+        channels[i].count = cnt;
+        channels[i].current = 0;
+        channels[i].bars = (Bar **) malloc(sizeof(Bar *) * cnt);
+        for (j = 0; j < cnt; j++) {
+            channels[i].bars[j] = &bars[CHANNELS[counter++]];
+            //counter += sizeof(uint16_t);
+        }
+    }
 
-  counter = 0;
-  for (int i = 0; i < PROGRAM_COUNT; i++) {
-      uint16_t cnt = PROGRAMS[counter++];
-      //counter += sizeof(uint16_t);
-      programs[i].count = cnt;
-      programs[i].mode = 0;
-      programs[i].channels = (Channel**)malloc(sizeof(Channel*)*cnt);
-      for (int j = 0; j < cnt; j++) {
-        programs[i].channels[j] = &channels[PROGRAMS[counter++]];
+    counter = 0;
+    for (i = 0; i < PROGRAM_COUNT; i++) {
+        uint16_t cnt = PROGRAMS[counter++];
         //counter += sizeof(uint16_t);
-      }
-  }
+        programs[i].count = cnt;
+        programs[i].mode = 0;
+        programs[i].channels = (Channel **) malloc(sizeof(Channel *) * cnt);
+        for (j = 0; j < cnt; j++) {
+            programs[i].channels[j] = &channels[PROGRAMS[counter++]];
+            //counter += sizeof(uint16_t);
+        }
+    }
 
-  data->programCount = PROGRAM_COUNT;
-  data->programs = programs;
+    data->programCount = PROGRAM_COUNT;
+    data->programs = programs;
 
-  return data;
+    return data;
 }
 
-void reset_program(Composition* prog) {
-    for (int i = 0; i < prog->count; i++) {
+void reset_program(Composition *prog) {
+    int i,j;
+    for (i = 0; i < prog->count; i++) {
         Channel *chan = prog->channels[i];
         chan->current = 0;
-        for (int j = 0; j < chan->count; j++) {
+        for (j = 0; j < chan->count; j++) {
             Bar *bar = chan->bars[j];
             bar->current = 0;
         }
